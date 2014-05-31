1a2e145d696d5f80663641c198cf3fe93ed86c7d
Seena Burns <hello@seenaburns.com>
2014-05-31 12:06:24 -0700

    Add binary example for testing

---
+++ examples/binary.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+OUT=/dev/null
+FREQUENCY=0.5
+
+if [[ -n "$1" ]]
+then
+    OUT=$1
+fi
+
+while true;
+do
+    rand=$[ RANDOM % 5 ]
+    if [ $rand -eq 0 ]
+    then
+        echo 1.0
+    else
+        echo 0.0
+    fi
+    sleep $FREQUENCY;
+done | tee $OUT | ./stag
