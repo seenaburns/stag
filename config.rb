c447402584a59f18beb79da4a381e3794b85c45e
Seena Burns <hello@seenaburns.com>
2014-08-26 18:55:38 -0700

    Modified midnight theme for stag

---
+++ config.rb
@@ -0,0 +1,24 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "stylesheets"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :expanded
+environment = :production
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass stylesheets scss && rm -rf sass && mv scss sass
